d2fc5c5e58a1c8e225feae44c1c153ea8701338f
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-07-02 22:07:07 +0200

    PATCH /posts/:id done

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,10 +1,34 @@
 class Api::V1::PostsController < Api::V1::BaseController
+  before_action :set_post, only: [ :show, :update ]
 
   def index
     @posts = Post.all
   end
 
   def show
+  end
+
+  def update
+    if @post.update(post_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
   end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def render_error
+    render json: { errors: @post.errors.full_messages },
+     status: :unprocessable_entity
+  end
+
 end
