6deef1638f13f2e43222dc74f04cf501e3f9488a
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2023-03-31 23:01:04 +0200

    added positions to posts to fix frontend list issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+
+  default_scope { order(position: :asc) }
+
   has_many :comments, dependent: :destroy
   has_many :subcomments, through: :appointments
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_181447) do
+ActiveRecord::Schema.define(version: 2023_03_31_210024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_07_02_181447) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
   end
 
   create_table "subcomments", force: :cascade do |t|
