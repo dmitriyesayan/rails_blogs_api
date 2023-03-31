a09ae9166ac76804c07a97b832de6bf68cbdbb4d
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2023-03-31 22:44:01 +0200

    modified config/application.js to allow API calls from servers

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,11 @@ module PostsApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:8080'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
